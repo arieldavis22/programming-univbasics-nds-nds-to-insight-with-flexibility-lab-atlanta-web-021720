1e74c8b9fcafb116510b7e8e937cf1d10b284170
Ariel Davis <aadavisbusiness@gmail.com>
2020-01-21 16:32:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,6 @@
 # Provided, don't edit
 require 'directors_database'
+require 'pry'
 
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
 # [3,4,5], [6]] => [1,2,3,4,5,6].
@@ -21,7 +22,7 @@ def flatten_a_o_a(aoa)
 end
 
 def movie_with_director_name(director_name, movie_data)
-  { 
+  {
     :title => movie_data[:title],
     :worldwide_gross => movie_data[:worldwide_gross],
     :release_year => movie_data[:release_year],
@@ -48,6 +49,14 @@ def movies_with_director_key(name, movies_collection)
   # Array of Hashes where each Hash represents a movie; however, they should all have a
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
+
+  movie_arr = []
+  movie_index = 0
+  while movie_index < movies_collection.length do
+    movie_arr << movie_with_director_name(name, movies_collection[movie_index])
+    movie_index += 1
+  end
+  movie_arr
 end
 
 
@@ -76,6 +85,17 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+movie_arr = []
+movie_index = 0
+binding.pry
+while movie_index < source.length do
+  movie_arr << source[movie_index][:movies]
+  movie_index += 1
+end
+# movie_inner = 0
+# while movie_inner < movie_arr.length do
+#   movie_arr[movie_inner]
+# end
 end
 
 # ----------------    End of Your Code Region --------------------
